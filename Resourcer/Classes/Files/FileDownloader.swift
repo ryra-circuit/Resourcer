3c2713983b902b4561f11330c6e53e3b6ab0d053
Chandima Dasanayaka <chandima.emedia@gmail.com>
2020-01-03 18:08:17 +0530

    Updated file downloader

--- Resourcer/Classes/Files/FileDownloader.swift
+++ Resourcer/Classes/Files/FileDownloader.swift
@@ -1,31 +1,46 @@
 import UIKit
 import Alamofire
 
-public typealias fileDownloadHandler = (_ status: Bool, _ message: String, _ url: URL?) -> ()
+public typealias fileDownloadHandler = (_ status: Bool, _ message: String,_ url: URL?) -> ()
+
+var sessionManager: SessionManager = {
+  let configuration = URLSessionConfiguration.default
+  configuration.timeoutIntervalForRequest = 20
+  return SessionManager(configuration: configuration)
+}()
 
 public protocol FileDownloaderDelegate {
-    func downloadFileAndSaveToDocuments(from url: String, with name: String, completion: @escaping fileDownloadHandler)
+    func downloadFileAndSaveToDocuments(from url: String, with name: String, config: URLSessionConfiguration, parameters: [String: Any], method: HTTPMethod, encoding: URLEncoding, headers: HTTPHeaders, timeout: Double, completion: @escaping fileDownloadHandler)
 }
 
 public extension FileDownloaderDelegate {
     
-    func downloadFileAndSaveToDocuments(from url: String, with name: String, completion: @escaping fileDownloadHandler) {
-        Alamofire.request(url).downloadProgress(closure : { (progress) in
-            print(progress.fractionCompleted)
-
-        }).responseData{ (response) in
-
-            if let data = response.result.value {
-
+    func downloadFileAndSaveToDocuments(from url: String, with name: String, config: URLSessionConfiguration = .default, parameters: [String: Any] = [:], method: HTTPMethod = .get, encoding: URLEncoding = .default, headers: HTTPHeaders = [:], timeout: Double = 30, completion: @escaping fileDownloadHandler) {
+        
+        //make session manager and assign
+        sessionManager = {
+            let configuration = config
+            configuration.timeoutIntervalForRequest = timeout
+            return SessionManager(configuration: configuration)
+        }()
+        
+        sessionManager.request(url,method: method, parameters: parameters, encoding: encoding, headers: headers).downloadProgress(queue: .main) { progress in
+            print("Progress is \(progress.fractionCompleted)")
+        }.responseData { response in
+            
+            switch response.result {
+            case .success(let data):
                 let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
                 let fileURL = documentsURL.appendingPathComponent(name)
                 do {
                     try data.write(to: fileURL)
-                    completion(true, "File downloaded succesfully. You can find it at Device/Files App/On My iPhone/App/\(name).", fileURL)
+                    completion(true, "successfully written to \(fileURL.absoluteString)",fileURL)
 
                 } catch {
-                    completion(false, "Failed to downlod file.", nil)
+                    completion(false, "Failed to write the file.", nil)
                 }
+            case .failure(let error):
+                completion(false, "Failed to download file.)", nil)
             }
         }
     }
